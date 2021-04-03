cecd5cd8f2c8b57a477f4f964c0934611f108214
Pedro Marchana <pmarchena1@cuc.edu.co>
2021-04-03 15:31:18 -0500

    commit con los dos primeros ejercicios de la clase mas el cuarto ejercicio

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -1,2 +1,13 @@
 def convert(number):
-    pass
+    r = ""
+    if number % 3 == 0:
+        r += "Pling"
+    if number % 5 == 0:
+        r += "Plang"
+    if number % 7 == 0:
+        r += "Plong"
+
+    if len(r) != 0:
+        return r
+    else:
+        return str(number)
